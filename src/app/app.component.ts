82e1333d8a5795389ca7a4649f9814a63cc09c74
Pavel Prorovsky <prorovsky@gmail.com>
2017-08-02 20:08:00 +0300

    add framework skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/framework-config.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    constructor(private frameworkConfigService: FrameworkConfigService) {
+        const config: FrameworkConfigSettings = {
+            socialIcons: [
+                { imageFile: 'assets/social-fb-bw.jpg', alt: 'Facebook', link: 'https://www.facebook.com'},
+                { imageFile: 'assets/social-google-bw.jpg', alt: 'Google +', link: 'https://www.google.com'},
+                { imageFile: 'assets/social-twitter-bw.jpg', alt: 'Twitter', link: 'https://www.twitter.com'}
+            ],
+            showLanguageSelector: true,
+            showUserControls: true,
+            showStatusBar: true,
+            showStatusBarBreakpoint: 800
+        };
+
+        frameworkConfigService.configure(config);
+    }
+
 }
--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
+import { FrameworkConfigService } from './services/framework-config.service';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { StatusBarComponent } from './status-bar/status-bar.component';
 
 @NgModule({
     imports: [
@@ -12,7 +15,12 @@ import { TitleBarComponent } from './title-bar/title-bar.component';
     declarations: [
         FrameworkBodyComponent,
         ContentComponent,
-        TitleBarComponent
+        TitleBarComponent,
+        TopBarComponent,
+        StatusBarComponent
+    ],
+    providers: [
+        FrameworkConfigService
     ],
     exports: [FrameworkBodyComponent]
 })
